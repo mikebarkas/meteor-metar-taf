6e0f89129b85c0ee2327465ad4f29100ce613675
Mike Barkas <mike@barkas.com>
2015-05-01 23:27:07 -0400

    Method and rpc call for api.

--- meteor-metar-taf.js
+++ meteor-metar-taf.js
@@ -1,10 +1,23 @@
 if (Meteor.isClient) {
+
+  Meteor.call('checkWx', function (err, res) {
+//    if (err) {
+    console.log(res);
+//    }
+    Session.set('weather', res.content);
+  });
+
+  Template.wx.helpers({
+    metar: Session.get('weather')
+  });
+
 }
 
 if (Meteor.isServer) {
-  url = "http://aviationweather.gov/adds/dataserver_current/httpparam?dataSource=metars&requestType=retrieve&format=xml&stationString=KDEN&hoursBeforeNow=1";
-
-  wx = Meteor.http.get(url);
-  console.log(wx);
-
+  Meteor.methods({
+    checkWx: function () {
+      url = "http://aviationweather.gov/adds/dataserver_current/httpparam?dataSource=metars&requestType=retrieve&format=xml&stationString=KCLT&hoursBeforeNow=1";
+      return Meteor.http.get(url);
+    }
+  });
 }
